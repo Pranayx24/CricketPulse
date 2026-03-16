bdf811d1fc30a6310a48dced17e13356a5f9f69b
Pranayx24 <145815203+Pranayx24@users.noreply.github.com>
2026-03-17 00:42:45 +0530

    Update page.tsx

--- src/app/match/[id]/page.tsx
+++ src/app/match/[id]/page.tsx
@@ -20,35 +20,87 @@ export default function MatchPage({ params }: any) {
 
   if(!match){
     return(
-      <div style={{padding:"40px"}}>
-      Loading match...
+      <div style={{padding:"40px",color:"white"}}>
+        Loading match...
       </div>
     )
   }
 
   return(
 
-    <div style={{padding:"40px"}}>
+    <div style={{padding:"40px",color:"white"}}>
 
-      <h1>{match.name}</h1>
+      {/* MATCH HEADER */}
 
-      <h2 style={{color:"#00ff88"}}>
-      {match.status}
-      </h2>
+      <div
+      style={{
+        border:"2px solid red",
+        padding:"25px",
+        borderRadius:"12px",
+        boxShadow:"0 0 20px rgba(255,0,0,0.6)",
+        marginBottom:"30px"
+      }}
+      >
 
-      <br/>
+        <h1 style={{fontSize:"30px"}}>
+          {match.name}
+        </h1>
 
-      <p>Match Type: {match.matchType}</p>
-      <p>Venue: {match.venue}</p>
-      <p>Date: {match.date}</p>
+        <p style={{color:"#00ff88",fontSize:"18px"}}>
+          {match.status}
+        </p>
 
-      <br/>
+      </div>
 
-      <h2>Teams</h2>
+
+      {/* TEAMS SECTION */}
+
+      <div
+      style={{
+        display:"grid",
+        gridTemplateColumns:"1fr 1fr",
+        gap:"20px",
+        marginBottom:"30px"
+      }}
+      >
 
       {match.teamInfo?.map((team:any,index:number)=>(
-        <p key={index}>{team.name}</p>
+
+        <div
+        key={index}
+        style={{
+          border:"2px solid red",
+          padding:"20px",
+          borderRadius:"10px"
+        }}
+        >
+
+        <h2>{team.name}</h2>
+
+        </div>
+
       ))}
+
+      </div>
+
+
+      {/* MATCH INFO */}
+
+      <div
+      style={{
+        border:"2px solid red",
+        padding:"25px",
+        borderRadius:"10px"
+      }}
+      >
+
+      <h2 style={{marginBottom:"15px"}}>Match Information</h2>
+
+      <p><b>Match Type:</b> {match.matchType}</p>
+      <p><b>Venue:</b> {match.venue}</p>
+      <p><b>Date:</b> {match.date}</p>
+
+      </div>
 
     </div>
 
