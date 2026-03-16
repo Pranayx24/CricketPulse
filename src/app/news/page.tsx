87e85b472b683ffa359fa1959e92c0e2924e80c2
Pranayx24 <145815203+Pranayx24@users.noreply.github.com>
2026-03-17 01:20:08 +0530

    Update page.tsx

--- src/app/news/page.tsx
+++ src/app/news/page.tsx
@@ -1,8 +1,17 @@
-export default function NewsPage() {
-  return (
-    <div style={{padding:"40px"}}>
-      <h1>Cricket News</h1>
-      <p>Latest cricket news will appear here.</p>
-    </div>
-  )
+export default function News(){
+
+return(
+
+<div style={{padding:"40px"}}>
+
+<h1 style={{fontSize:"40px"}}>Cricket News</h1>
+
+<p style={{color:"#00ff88"}}>
+News section coming soon...
+</p>
+
+</div>
+
+)
+
 }
