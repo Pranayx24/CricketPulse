be597cedf8c49325a83c6ff526a097edfbe8f60a
Pranayx24 <145815203+Pranayx24@users.noreply.github.com>
2026-03-17 01:32:51 +0530

    Update route.ts

--- src/app/api/matches/route.ts
+++ src/app/api/matches/route.ts
@@ -4,15 +4,20 @@ export async function GET() {
 
   const API_KEY = "2bb0be13-6bdd-421f-9786-41f590656393"
 
-  const res = await fetch(
-    `https://api.cricapi.com/v1/matches?apikey=${API_KEY}&offset=0`,
-    {
-      next: { revalidate: 60 } // cache for 60 seconds
-    }
-  )
+  try {
 
-  const data = await res.json()
+    const res = await fetch(
+      `https://api.cricapi.com/v1/matches?apikey=${API_KEY}`
+    )
 
-  return NextResponse.json(data)
+    const data = await res.json()
+
+    return NextResponse.json(data)
+
+  } catch (error) {
+
+    return NextResponse.json({ error: "API failed" })
+
+  }
 
 }
