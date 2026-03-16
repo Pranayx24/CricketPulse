8c192c87ca07a7d31be22c8b162343421815113a
Pranayx24 <145815203+Pranayx24@users.noreply.github.com>
2026-03-17 00:39:06 +0530

    Update page.tsx

--- src/app/match/[id]/page.tsx
+++ src/app/match/[id]/page.tsx
@@ -2,50 +2,52 @@
 
 import { useEffect, useState } from "react"
 
-export default function MatchDetail({ params }: any) {
+export default function MatchPage({ params }: any) {
 
   const API_KEY = "2bb0be13-6bdd-421f-9786-41f590656393"
 
-  const [match, setMatch] = useState<any>(null)
+  const [match,setMatch] = useState<any>(null)
 
-  useEffect(() => {
+  useEffect(()=>{
 
     fetch(`https://api.cricapi.com/v1/match_info?apikey=${API_KEY}&id=${params.id}`)
-      .then(res => res.json())
-      .then(data => {
-        setMatch(data.data)
-      })
+    .then(res=>res.json())
+    .then(data=>{
+      setMatch(data.data)
+    })
 
-  }, [])
+  },[])
 
   if(!match){
-    return <div style={{padding:"40px"}}>Loading match...</div>
+    return(
+      <div style={{padding:"40px"}}>
+      Loading match...
+      </div>
+    )
   }
 
-  return (
+  return(
 
     <div style={{padding:"40px"}}>
 
       <h1>{match.name}</h1>
 
       <h2 style={{color:"#00ff88"}}>
-        {match.status}
+      {match.status}
       </h2>
 
       <br/>
 
-      <p><b>Match Type:</b> {match.matchType}</p>
-      <p><b>Venue:</b> {match.venue}</p>
-      <p><b>Date:</b> {match.date}</p>
+      <p>Match Type: {match.matchType}</p>
+      <p>Venue: {match.venue}</p>
+      <p>Date: {match.date}</p>
 
       <br/>
 
       <h2>Teams</h2>
 
       {match.teamInfo?.map((team:any,index:number)=>(
-        <div key={index}>
-          <p>{team.name}</p>
-        </div>
+        <p key={index}>{team.name}</p>
       ))}
 
     </div>
